2e91b894954baa83eea577e720dc97bfcb280083
Phil de Joux <philderbeast@gmail.com>
2013-04-05 16:37:05 +1000

    Use bootstrap, update default template.
    Wipe clean the default CSS. Format site.hs. Add a batch file for setting
    the code page in the DOS shell. Add sample posts with fsharp and haskell
    code. Add a gitignore. Add CSS for pandoc, solarized, bootswatch
    readable and font awesome. Update the default template to use bootstrap.
    Add the Block Scope logo as a asciimathml formula.

--- CampHog/site.hs
+++ CampHog/site.hs
@@ -1,31 +1,39 @@
---------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
-import           Control.Applicative ((<$>))
-import           Data.Monoid         (mappend)
-import           Hakyll
+import Control.Applicative ((<$>))
+import Data.Monoid (mappend)
+import Hakyll
+import Hakyll.Core.Item
+import Hakyll.Web.Pandoc
+import Text.Pandoc.Options
 
+pandocReaderOptions :: ReaderOptions
+pandocReaderOptions = defaultHakyllReaderOptions
 
---------------------------------------------------------------------------------
+pandocWriterOptions :: WriterOptions
+pandocWriterOptions = defaultHakyllWriterOptions
+    { writerHTMLMathMethod = MathJax ""
+    }
+
 main :: IO ()
 main = hakyll $ do
     match "images/*" $ do
-        route   idRoute
+        route idRoute
         compile copyFileCompiler
 
     match "css/*" $ do
-        route   idRoute
+        route idRoute
         compile compressCssCompiler
 
     match (fromList ["about.rst", "contact.markdown"]) $ do
-        route   $ setExtension "html"
-        compile $ pandocCompiler
+        route $ setExtension "html"
+        compile $ pandocCompilerWith pandocReaderOptions pandocWriterOptions
             >>= loadAndApplyTemplate "templates/default.html" defaultContext
             >>= relativizeUrls
 
     match "posts/*" $ do
         route $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
+        compile $ pandocCompilerWith pandocReaderOptions pandocWriterOptions
+            >>= loadAndApplyTemplate "templates/post.html" postCtx
             >>= loadAndApplyTemplate "templates/default.html" postCtx
             >>= relativizeUrls
 
@@ -34,20 +42,17 @@ main = hakyll $ do
         compile $ do
             let archiveCtx =
                     field "posts" (\_ -> postList recentFirst) `mappend`
-                    constField "title" "Archives"              `mappend`
+                    constField "title" "Archives" `mappend`
                     defaultContext
-
             makeItem ""
                 >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
                 >>= loadAndApplyTemplate "templates/default.html" archiveCtx
                 >>= relativizeUrls
 
-
     match "index.html" $ do
         route idRoute
         compile $ do
             let indexCtx = field "posts" $ \_ -> postList (take 3 . recentFirst)
-
             getResourceBody
                 >>= applyAsTemplate indexCtx
                 >>= loadAndApplyTemplate "templates/default.html" postCtx
@@ -55,18 +60,12 @@ main = hakyll $ do
 
     match "templates/*" $ compile templateCompiler
 
-
---------------------------------------------------------------------------------
 postCtx :: Context String
-postCtx =
-    dateField "date" "%B %e, %Y" `mappend`
-    defaultContext
+postCtx = dateField "date" "%B %e, %Y" `mappend` defaultContext
 
-
---------------------------------------------------------------------------------
 postList :: ([Item String] -> [Item String]) -> Compiler String
 postList sortFilter = do
-    posts   <- sortFilter <$> loadAll "posts/*"
+    posts <- sortFilter <$> loadAll "posts/*"
     itemTpl <- loadBody "templates/post-item.html"
-    list    <- applyTemplateList itemTpl postCtx posts
+    list <- applyTemplateList itemTpl postCtx posts
     return list
