d11463cfe0a37a1703625f4bc00c4a19b1fc22e7
Phil de Joux <philderbeast@gmail.com>
2021-07-30 14:38:07 -0400

    Bump fontawesome version.

--- app.js
+++ app.js
@@ -2,3 +2,8 @@
 
 require('./app.html');
 require('./app.sass');
+
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
